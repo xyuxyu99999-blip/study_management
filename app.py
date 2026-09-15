126e06419efad6881f3c60c30970376ad524d1bc
xyuxyu99999-blip <xyuxyu99999@gmail.com>
2026-09-15 18:23:40 +0900

    Remove Flask app run configuration
    Removed Flask app run configuration from app.py.

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import os
 import streamlit as st
 
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
 
 # スマホでタップした瞬間にサイドバーを強制的に閉じるためのJavaScriptを仕込む
 # ラジオボタンがクリックされたら、Streamlit標準の「サイドバーを閉じるボタン」を自動でクリックさせます
